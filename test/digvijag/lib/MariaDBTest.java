1f4bcfc6d88d497355cf48c2d0d85cb891350195
Digvijay <digvijag@thoughtworks.com>
2014-01-27 10:43:32 +0530

    select tested.

--- test/digvijag/lib/MariaDBTest.java
+++ test/digvijag/lib/MariaDBTest.java
@@ -4,7 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 
 import static junit.framework.Assert.assertEquals;
@@ -18,19 +21,19 @@ public class MariaDBTest {
     public void setUp() throws Exception {
         connection = mariadbConnection.getDbConnection();
         statement = connection.createStatement();
-        statement.execute("create table DDLTest(columnOne int,columnTwo varchar(20));");
+        statement.execute("create table DMLTest(columnOne int,columnTwo varchar(20));");
     }
 
     @After
     public void tearDown() throws Exception {
-        statement.execute("drop table DDLTest");
+        statement.execute("drop table DMLTest");
         statement.close();
         mariadbConnection.closeConnection(connection);
     }
 
     @Test
     public void test_insert_one_row_in_table() throws SQLException {
-        String query = "insert into DDLTest values(1,\"test\");";
+        String query = "insert into DMLTest values(1,\"test\");";
         int expected = 1;
         int actual = statement.executeUpdate(query);
         assertEquals(expected, actual);
@@ -38,12 +41,38 @@ public class MariaDBTest {
 
     @Test
     public void test_insert_multiples_row_in_table() throws SQLException {
-        String query1 = "insert into DDLTest values(1,\"test\");";
-        String query2 = "insert into DDLTest values(2,\"test2\");";
+        String query1 = "insert into DMLTest values(1,\"test\");";
+        String query2 = "insert into DMLTest values(2,\"test2\");";
         int[] expected = {1, 1};
         statement.addBatch(query1);
         statement.addBatch(query2);
         int[] actual = statement.executeBatch();
-        assert(Arrays.equals(expected, actual));
+        assert (Arrays.equals(expected, actual));
+    }
+
+    @Test
+    public void test_delete_multiples_row_in_table() throws SQLException {
+        String query1 = "insert into DMLTest values(1,\"test\");";
+        String query2 = "insert into DMLTest values(2,\"test2\");";
+        int[] expected = {1, 1};
+        statement.addBatch(query1);
+        statement.addBatch(query2);
+        int[] actual = statement.executeBatch();
+        assert (Arrays.equals(expected, actual));
+    }
+
+    @Test
+    public void test_select_from_table() throws SQLException {
+        String insertQuery = "insert into DMLTest values(1,\"test\");";
+        String selectQuery = "select * from DMLTest";
+        String expected = "test";
+        String actual = null;
+        statement.executeQuery(insertQuery);
+        ResultSet resultSet = statement.executeQuery(selectQuery);
+        while (resultSet.next()){
+            actual = resultSet.getString(2);
+        }
+        resultSet.close();
+        assertEquals(expected, actual);
     }
 }
